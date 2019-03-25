803bf457e9adb685ad3894cda154ffc39c1fada9
GPeraltP <45854786+GPeraltP@users.noreply.github.com>
2019-03-25 18:42:19 -0500

    Update openNativeSettings.js

--- www/openNativeSettings.js
+++ www/openNativeSettings.js
@@ -5,5 +5,3 @@ NativeSettings.open = function(setting, onsucess, onfail) {
 	var settings = (typeof setting === 'string' || setting instanceof String) ? [setting] : setting;
 	cordova.exec(onsucess, onfail, "NativeSettings", "open", settings);
 };
-
-module.exports = NativeSettings;
